c138fd7d75894ffece8a4fcbac4358cee0e88afb
veccv <veccv@github.com>
2024-02-01 12:57:39 +0100

    Added scheduled task to update last song played and notify channel in SongScheduler, and added lastSong to UserConfig

--- src/main/kotlin/com/github/veccvs/djforsenbotkotlin/component/SongScheduler.kt
+++ src/main/kotlin/com/github/veccvs/djforsenbotkotlin/component/SongScheduler.kt
@@ -1,7 +1,10 @@
 package com.github.veccvs.djforsenbotkotlin.component
 
+import com.github.veccvs.djforsenbotkotlin.config.UserConfig
 import com.github.veccvs.djforsenbotkotlin.dao.CytubeDao
+import com.github.veccvs.djforsenbotkotlin.model.LastSong
 import com.github.veccvs.djforsenbotkotlin.repository.SongRepository
+import com.github.veccvs.djforsenbotkotlin.service.CommandService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -10,6 +13,8 @@ import org.springframework.stereotype.Component
 class SongScheduler(
   @Autowired private val cytubeDao: CytubeDao,
   @Autowired private val songRepository: SongRepository,
+  @Autowired private val userConfig: UserConfig,
+  @Autowired private val commandService: CommandService,
 ) {
   @Scheduled(fixedDelay = 2000)
   fun scheduleSong() {
@@ -19,6 +24,34 @@ class SongScheduler(
         cytubeDao.getPlaylist()?.queue?.isEmpty() == true
     ) {
       songRepository.findAll().random().link?.let { cytubeDao.addVideo(it) }
+    }
+  }
+
+  @Scheduled(fixedDelay = 2000)
+  fun changeLastSong() {
+    if (
+      cytubeDao.getBotStatus() != null &&
+        cytubeDao.getBotStatus()!!.botEnabled &&
+        cytubeDao.getPlaylist()?.queue?.isEmpty() == false
+    ) {
+      if (userConfig.lastSong.link == "") {
+        userConfig.lastSong = LastSong().apply { link = cytubeDao.getPlaylist()!!.queue[0].link.id }
+      } else if (cytubeDao.getPlaylist()!!.queue[0].link.id != userConfig.lastSong.link) {
+        userConfig.lastSong = LastSong().apply { link = cytubeDao.getPlaylist()!!.queue[0].link.id }
+        if (cytubeDao.getPlaylist()!!.queue[0].title.length < 50) {
+          commandService.sendMessage(
+            userConfig.channelName!!,
+            "pepeJAM now playing: ${cytubeDao.getPlaylist()!!.queue[0].title.substring(0,
+                            cytubeDao.getPlaylist()!!.queue[0].title.length,
+                            )}",
+          )
+        } else {
+          commandService.sendMessage(
+            userConfig.channelName!!,
+            "pepeJAM now playing: ${cytubeDao.getPlaylist()!!.queue[0].title.substring(0, 50)}[...]",
+          )
+        }
+      }
     }
   }
 }
--- src/main/kotlin/com/github/veccvs/djforsenbotkotlin/config/UserConfig.kt
+++ src/main/kotlin/com/github/veccvs/djforsenbotkotlin/config/UserConfig.kt
@@ -1,5 +1,6 @@
 package com.github.veccvs.djforsenbotkotlin.config
 
+import com.github.veccvs.djforsenbotkotlin.model.LastSong
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -10,4 +11,5 @@ class UserConfig {
   @Value("\${user.minutes-to-add-video}") val minutesToAddVideo: Int? = null
   @Value("\${user.seconds-to-response}") val secondsToResponseToCommand: Int? = null
   @Value("\${chat.channel-name}") val channelName: String? = null
+  var lastSong: LastSong = LastSong()
 }
